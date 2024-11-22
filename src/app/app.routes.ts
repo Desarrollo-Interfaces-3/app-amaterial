6aec3283866bcd355de7b3911c432adb8b1813fb
Desarrollo-Interfaces-3 <daplicacionesmoviles1@gmail.com>
2024-11-22 14:30:09 -0500

    ejemplo de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,27 @@
 import { Routes } from '@angular/router';
 import { EjemploAmaterialComponent } from './ejemplo-amaterial/ejemplo-amaterial.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './dashboard/home/home.component';
+import { CursolistaComponent } from './dashboard/curso/cursolista/cursolista.component';
+import { AlumnolistaComponent } from './dashboard/alumno/alumnolista/alumnolista.component';
+import { CursodetalleComponent } from './dashboard/curso/cursodetalle/cursodetalle.component';
+import { AlumnodetalleComponent } from './dashboard/alumno/alumnodetalle/alumnodetalle.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-    {path: "amaterial", component: EjemploAmaterialComponent}
+    {path: "amaterial", component: EjemploAmaterialComponent},
+    {path: "login", component: LoginComponent},
+    {path: "dashboard", component: DashboardComponent,
+        children: [
+            {path: "home", component: HomeComponent},
+            {path: "curso", component: CursolistaComponent},
+            {path: "alumno", component: AlumnolistaComponent},
+            {path: "curso/:id", component: CursodetalleComponent},
+            {path: "alumno/:id", component: AlumnodetalleComponent}
+        ]
+    },
+    {path: "", redirectTo:"login", pathMatch: "full"},
+    {path: "**", component: PageNotFoundComponent}
+
 ];
--- src/app/ejemplo-amaterial/ejemplo-amaterial.component.ts
+++ src/app/ejemplo-amaterial/ejemplo-amaterial.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 import { MaterialModule } from '../angular-material/material/material.module';
 
 
