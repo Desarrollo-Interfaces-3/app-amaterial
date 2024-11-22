8e204e21898474aa8089e9817e68df6e4f105fef
Desarrollo-Interfaces-3 <daplicacionesmoviles1@gmail.com>
2024-11-22 14:01:19 -0500

    agregando modulo para angular material

--- src/app/ejemplo-amaterial/ejemplo-amaterial.component.ts
+++ src/app/ejemplo-amaterial/ejemplo-amaterial.component.ts
@@ -3,12 +3,13 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MaterialModule } from '../angular-material/material/material.module';
 
 
 @Component({
   selector: 'app-ejemplo-amaterial',
   standalone: true,
-  imports: [MatButtonModule, MatDividerModule, MatIconModule, MatCardModule],
+  imports: [MaterialModule],
   templateUrl: './ejemplo-amaterial.component.html',
   styleUrl: './ejemplo-amaterial.component.css'
 })
